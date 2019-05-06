49e3c6fd3fc707afc4154450b7c0f87b844cbb3a
thomas <thomas.ng@isuntv-hk.comthomas.ng@isuntv-hk.comthomas.ng@isuntv-hk.comthomas.ng@isuntv-hk.comathomas.ng@isuntv-hk.comthomas.ng@isuntv-hk.comthomas.ng@isuntv-hk.comthomas.ng@isuntv-hk.com>
2019-05-06 19:05:55 +0800

    [master] doing the orders

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var express = require('express')
 var app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
+const axios = require('axios');
+require('dotenv').config()
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// config
+var config = {
+    google_key : process.env.google_key ? process.env.google_key : ""
+}
 
 // function initDb(){
 //     var mysql = require('mysql');
@@ -19,8 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/orders', function (req, res) {
     const {origin ,  destination} = req.body
-    console.log('origin' , origin);
-    console.log('destination' , destination);
 
     if(origin.length != 2 || destination.length != 2 ){
         res.send({ 
@@ -28,22 +33,32 @@ app.post('/orders', function (req, res) {
         }, 400)      
     }
 
-    // axios.post('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=${google_key}', {
-    //     firstName: 'Fred',
-    //     lastName: 'Flintstone'
-    //   })
-    //   .then(function (response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
+    axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Washington,DC&destinations=New+York+City,NY&key=${config.google_key}`)
+      .then(function (response) {
+        if(response.status == 200){
+            const { data } = response
+            let distance = data.rows[0] && data.rows[0].elements ? data.rows[0].elements[0].distance : null
+            res.send({ 
+                "id" : 1 ,
+                "distance" : distance.text ? parseInt(distance.text.replace(' km' , '')) : 0,
+                "status": "UNASSIGNED"
+            }, 200)
+        }else{
+            res.send({ 
+                code : 500,
+                error_code : "GOOGLE_API_ERROR",
+                message : "cannot call google api service"
+            }, 500)        
+        }
+      }).catch(function (error) {
+        console.log(error);
+        res.send({ 
+            code : 500,
+            error_code : "ERROR",
+            message : error
+        }, 500)        
+      });
 
-    res.send({ 
-        "id": 1,
-        "distance": "wow",
-        "status": "UNASSIGNED"
-    }, 200)
 })
 
 app.listen(8080);
